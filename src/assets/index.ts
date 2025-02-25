0f3c509624300f88911f71052cac68ce7eb06e21
Iuri Mendes <fcoiuri@gmail.com>
2025-02-25 17:04:49 -0300

    build(icons): create a component for each icon
    Configure each icon to have its own component to fix error of webpack during build.

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as FavoriteSVG } from '@/assets/icons/Favorite';
+export { default as FavoritedSVG } from '@/assets/icons/Favorited';
+export { default as HeartSVG } from '@/assets/icons/Heart';
+export { default as HomePageSVG } from '@/assets/icons/HomePageIcon';
+export { default as SearchSVG } from '@/assets/icons/SearchIcon';
+export { default as UserSVG } from '@/assets/icons/UserIcon';
+export { default as UserNotFoundSVG } from '@/assets/icons/UserNotFoundIcon';
