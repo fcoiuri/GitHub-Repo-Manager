138797862dc2c71621574e546fa92b81d7a54237
Iuri Mendes <fcoiuri@gmail.com>
2025-02-25 01:58:15 -0300

    test(notfoundpage): add tests for NotFound Page
    Validate the title, check if the svg is on the page;
    Validate all copies.

---
+++ src/__tests__/NotFound.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import NotFound from '@/app/users/[username]/not-found';
+import { describe } from 'node:test';
+
+describe('SearchBar', () => {
+  test('should render a title with user searched', () => {
+    render(<NotFound username="test" />);
+
+    const title = screen.getByTestId('title');
+    const notFoundSVG = screen.getByTestId('user-not-found-icon');
+    const noUser = screen.getByText('Nenhum usuário encontrado');
+    const checkLetter = screen.getByText(
+      'Verifique se a escrita está correta ou tente novamente'
+    );
+
+    expect(title).toBeInTheDocument();
+    expect(notFoundSVG).toBeInTheDocument();
+    expect(noUser).toBeInTheDocument();
+    expect(checkLetter).toBeInTheDocument();
+  });
+});
